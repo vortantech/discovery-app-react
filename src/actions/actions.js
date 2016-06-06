f413bd6a025e891c68b1820057268de371e0f1a4
Khaled Garbaya <khaled@contentful.com>
2016-06-06 11:00:19 +0200

    no need to repeat isPreview

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,6 +3,6 @@ import Dispatcher from '../dispatchers/Dispatcher'
 export function changeTokenType (isPreview) {
   Dispatcher.dispatch({
     type: 'API_SELECTION_CHANGE',
-    isPreview: isPreview
+    isPreview
   })
 }
