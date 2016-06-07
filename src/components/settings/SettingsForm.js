6e8458438bef88acb107f6781910b63055fb3ac5
Khaled Garbaya <khaled@contentful.com>
2016-06-07 14:58:41 +0200

    fixup! Apply text changes

--- src/components/settings/SettingsForm.js
+++ src/components/settings/SettingsForm.js
@@ -20,7 +20,7 @@ function SettingsForm ({
   }
   return (
     <div styleName='settings-container'>
-      <h3 styleName='settings-title'>Discovery for Web</h3>
+      <h3 styleName='settings-title'>Discovery Web App</h3>
       <form onSubmit={loadSpace} styleName='settings-form'>
         <div styleName='form-container'>
           <p styleName='error' style={errorDisplay}>{validationError}</p>
@@ -45,8 +45,8 @@ function SettingsForm ({
       </form>
       <div styleName='form-caption'>
         <section>
-          <h4>What is The Contentful Discovery for Web App?</h4>
-          <p>The Contentful Discovery for Web App gives you a quick and easy way to preview your content on a web environment, and explore the contents of your Spaces</p>
+          <h4>What is The Contentful Discovery Web App?</h4>
+          <p>The Contentful Discovery Web App gives you a quick and easy way to preview your content on a web environment, and explore the contents of your Spaces</p>
         </section>
         <section>
           <h4>Still don't have a Space?</h4>
