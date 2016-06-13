ecf3439af601d1a07f4af6c8b28c031270bf4254
Khaled Garbaya <khaled@contentful.com>
2016-06-13 12:54:27 +0200

    Add search functionality to the assets section of the app

--- src/components/assets/AssetsContainer.js
+++ src/components/assets/AssetsContainer.js
@@ -1,8 +1,11 @@
 import React, {createClass} from 'react'
 import {getClient} from '../../services/contentfulClient'
 import Assets from './Assets'
-
-export default createClass({
+import Search from './Search'
+import styles from './AssetsContainer.css'
+import CSSModules from 'react-css-modules'
+import update from 'react-addons-update'
+const AssetsContainer =  createClass({
   getInitialState () {
     return {
       assets: {},
@@ -13,20 +16,37 @@ export default createClass({
   componentDidMount () {
     getClient().getAssets()
     .then((assets) => {
+      this.initialAssets = assets.toPlainObject()
       this.setState({
         assets: assets.toPlainObject(),
         phase: 'loaded'
       })
     })
   },
-
+  onChangeHandler (filter){
+    filter = filter.trim().toLowerCase()
+    const newItems = this.initialAssets.items.filter((asset)=>{
+      return asset.fields.title.trim().toLowerCase().match(filter)
+    })
+    const newState = update(this.state, {
+     assets : {
+       items : {
+         $set:newItems
+       }
+     }
+    });
+    this.setState(newState)
+  },
   render () {
     if (this.state.phase === 'loading') {
-      return <p>Loading your Assets...</p>
+      return <div styleName='assets'><p>Loading your Assets...</p></div>
     } else if (this.state.assets.length === 0) {
-      return <p>No assets are available.</p>
-    } else {
-      return <Assets items={this.state.assets.items} location={this.props.location}/>
+      return <div styleName='assets'><p>No assets are available.</p></div>
     }
+    return <div styleName='assets'>
+            <Search itemCount={this.state.assets.items.length} label='media assets' onChange={this.onChangeHandler}/>
+            <Assets items={this.state.assets.items} location={this.props.location}/>
+          </div>
   }
 })
+export default CSSModules(AssetsContainer, styles)
