03e0302334f48f98e9ac62bc2f687bf7e8183fc2
Khaled Garbaya <khaled@contentful.com>
2016-07-06 12:09:02 +0200

    Fix Edit in Contentful link

--- src/components/entries/Entry.js
+++ src/components/entries/Entry.js
@@ -22,7 +22,7 @@ function Entry ({entry, location}) {
           target='_blank'
           href={'https://app.contentful.com/spaces/' +
           location.query.space_id + '/entries/' +
-          location.pathname.split('/')[3]}>Edit in Contentful Web App</a>
+          entry.sys.id}>Edit in Contentful Web App</a>
       </section>
       <Field key={displayField.id} definition={displayField} content={entry.fields[displayField.id]} styleName='field' location={location}/>
       {remainingFields}
