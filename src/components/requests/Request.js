ec610e1d899fc3892f71253494a1ea9b832534db
Khaled Garbaya <khaled@contentful.com>
2016-07-11 13:55:12 +0200

    Show both raw parsed json output in the request history section

--- src/components/requests/Request.js
+++ src/components/requests/Request.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import JSONTree from 'react-json-tree'
+import Tabs from '../tabs/Tabs'
+import Pane from '../tabs/Pane'
 export default function Request ({request, location}) {
   return (
     <div>
-      <JSONTree data={request.rawPayload} />
+      <Tabs selected={0}>
+        <Pane label='Raw JSON Output'>
+          <div><JSONTree data={request.rawPayload} /></div>
+        </Pane>
+        <Pane label='Contentful.js Output'>
+          <div><JSONTree data={request.parsedPayload} /></div>
+        </Pane>
+      </Tabs>
+
     </div>
   )
 }
