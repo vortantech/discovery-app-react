786dc103a88fa4c96213974e7b046d846df763a1
Khaled Garbaya <khaled@contentful.com>
2016-07-29 11:04:28 +0200

    Make it clear which request is currently selected

--- src/components/requests/RequestListItem.js
+++ src/components/requests/RequestListItem.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router'
-// import {current} from '../List.css'
+import {current} from '../List.css'
+
 export default function RequestListItem ({item, i, location}) {
+
   function getClassIfCurrentlySelected () {
+    const pathnames = location.pathname.split('/')
+    if (pathnames.indexOf(`${i}`) >= 0) {
+      return current
+    }
     return ''
-  }
+	}
+
   return (
     <li className={getClassIfCurrentlySelected()}>
       <Link to={{pathname: `/requests/${i}`, query: location.query}}>{`[${item.time}] ${item.path}`}</Link>
